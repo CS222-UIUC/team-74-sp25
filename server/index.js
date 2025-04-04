9ab366d22407aabaf784836cf3191c7cd1a8c484
ddai20 <ddai20@illinois.edu>
2025-04-04 18:08:53 -0500

    created basic server responses

--- server/index.js
+++ server/index.js
@@ -1,4 +1,27 @@
-import "express";
-import "axios";
+import express, { json } from "express";
+import axios from "axios";
+import SkincareProduct from "./SkincareProduct.cjs";
 
-console.log("Hello, world!");
+let products = [];
+
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello world!");
+});
+
+app.post("/products", function (req, res) {
+  let data = req.query.hello;
+  //pass the quiz answers into the decision tree to get products
+  if ((data = "bye")) {
+    products.push(new SkincareProduct());
+  }
+
+  res.send("bye");
+});
+
+app.get("/products", function (req, res) {
+  res.send(JSON.stringify(products));
+});
+
+app.listen(3000);
