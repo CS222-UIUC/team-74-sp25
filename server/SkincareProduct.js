792d00feabd39e9c417bee65d446cd770a2ab507
ddai20 <143148099+ddai20@users.noreply.github.com>
2025-04-08 21:57:46 -0500

    created basic server responses (#2)
    * created basic server responses
    
    * cors enabled

--- server/SkincareProduct.js
+++ server/SkincareProduct.js
@@ -1,27 +1,19 @@
 class SkincareProduct {
-  TYPE = {
-    cleanser: "cleanser",
-    active: "active",
-    moisturizer: "moisturizer",
-  };
-
-  constructor(name, brand, type, ingredients, description, price) {
-    this.name = name;
-    this.brand = brand;
-    this.type = type; // Type of product (cleanser, active, moisturizer)
-    this.ingredients = ingredients;
-    this.description = description;
-    this.price = price;
-  }
-
-  // Method to display product details
-  getProductDetails() {
-    return `Product: ${this.name}\nBrand: ${this.brand}\nType: ${
-      this.type
-    }\nActive Ingredients: ${this.ingredients.join(
-      ", "
-    )}\nPrice: $${this.price.toFixed(2)}`;
-  }
+    constructor(
+        name = "a",
+        brand = "b",
+        type = this.TYPE.active,
+        ingredients = "c",
+        description = "d",
+        price = 0.0
+    ) {
+        this.name = name;
+        this.brand = brand;
+        this.type = type; //cleanser, active, moisturizer
+        this.ingredients = ingredients;
+        this.description = description;
+        this.price = price;
+    }
 }
 
 module.exports = SkincareProduct;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
-import "express";
-import "axios";
+const express = require("express");
+const SkincareProduct = require("./SkincareProduct");
+const cors = require("cors");
 
-console.log("Hello, world!");
+let products = [];
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/quiz_answers", function (req, res) {
+    //pass the quiz answers into the decision tree to get products
+
+    res.send("accepted");
+});
+
+app.get("/products", function (req, res) {
+    res.send(JSON.stringify(products));
+});
+
+app.listen(port, () => console.log("Running server on port: " + port));
