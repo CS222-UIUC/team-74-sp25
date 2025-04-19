36a121ee9c79d462c7f3da819ba4cfcbd27e47a6
ddai20 <ddai20@illinois.edu>
2025-04-18 21:39:57 -0500

    sent skin_type answer to server

--- server/index.js
+++ server/index.js
@@ -2,18 +2,26 @@ const express = require("express");
 const SkincareProduct = require("./SkincareProduct");
 const cors = require("cors");
 
+let skin_type = "";
 let products = [];
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/quiz_answers", function (req, res) {
-    //pass the quiz answers into the decision tree to get products
+app.get("/", function (req, res) {
+    skin_type = "";
+    products = [];
+    res.send("reset");
+});
+
+app.post("/skin_type", function (req, res) {
+    skin_type = req.body["skin_type"];
 
     res.send("accepted");
+    console.log(skin_type);
 });
 
 app.get("/products", function (req, res) {
