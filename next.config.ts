a6c65120dcff564c585febd6ecbd87a4b361c0c3
Fenghai Li <111167815+4R5T@users.noreply.github.com>
2025-05-13 01:27:00 -0500

    fix: fix yml and config.ts
    * fix
    
    * Add .nojekyll to fix
    
    * fix

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/TEAM-74-SP25' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/TEAM-74-SP25/' : '',
   images: {
     domains: ['m.media-amazon.com', 'a.media-amazon.com'],
-    unoptimized: true, 
+    unoptimized: true,
   },
   trailingSlash: true,
 };
