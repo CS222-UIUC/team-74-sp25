0b96165592e5b54ae1140f9374e6928a9e9b441f
Fenghai Li <111167815+4R5T@users.noreply.github.com>
2025-05-13 01:27:38 -0400

    Revert "Feat: deploy to github page (#15)"
    This reverts commit b488539f15dd5e52bc94e6c47cfc1cc84b223618.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/team-74-sp25' : '',
   images: {
     domains: ['m.media-amazon.com'],
-    unoptimized: true, 
   },
 };
 
