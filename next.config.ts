a7edb85aac4e3f59c3a160404881d1820421cb03
4R5T <maxmeetsli@gmail.com>
2025-05-13 00:23:41 -0500

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  // output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/team-74-sp25' : '',
   images: {
     domains: ['m.media-amazon.com'],
-    unoptimized: true,
+    unoptimized: true, 
   },
 };
 
