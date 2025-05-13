21a74f2d975c1dae7b84a81d2f4695edffe8413e
4R5T <maxmeetsli@gmail.com>
2025-05-13 00:31:42 -0500

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/team-74-sp25' : '',
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/TEAM-74-SP25' : '',
   images: {
     domains: ['m.media-amazon.com'],
     unoptimized: true, 
