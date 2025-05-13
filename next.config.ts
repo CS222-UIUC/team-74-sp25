4e35400a204eb5bb3a69c90cb3486d2045ca4341
4R5T <maxmeetsli@gmail.com>
2025-05-13 01:42:20 -0500

    test

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/TEAM-74-SP25' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/TEAM-74-SP25/' : '',
   images: {
     domains: ['m.media-amazon.com', 'a.media-amazon.com'],
     unoptimized: true,
